3dcaf736ae686a9fb700e38cfee17d2133135854
Kaique Munhoz <munhoz13km@gmail.com>
2023-03-29 07:54:13 -0300

    feat: Create categoryMeals filter/where

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ryc_meals/data/dummy_data.dart';
 import 'package:ryc_meals/models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
@@ -9,6 +10,10 @@ class CategoriesMealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context).settings.arguments as Category;
+
+    final categoryMeals = DUMMY_MEALS
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
 
     return Scaffold(
       appBar: AppBar(
