dc69cd646071f18b4cab59d8a59d756f818b38bf
Kaique Munhoz <munhoz13km@gmail.com>
2023-03-28 21:48:34 -0300

    feat: Wrap CategoriesScreen with a Scaffold

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -7,16 +7,18 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES
+            .map((dummyCategory) => CategoryItem(dummyCategory))
+            .toList(),
       ),
-      children: DUMMY_CATEGORIES
-          .map((dummyCategory) => CategoryItem(dummyCategory))
-          .toList(),
     );
   }
 }
