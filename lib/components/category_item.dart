0dacd9be1ef42018860701fbf27b24292a3e368e
Kaique Munhoz <munhoz13km@gmail.com>
2023-03-28 22:36:49 -0300

    feat: Wrap CategoryItem with InkWell

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -11,22 +11,24 @@ class CategoryItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return InkWell(
       child: Container(
-        padding: const EdgeInsets.all(15.0),
-        child: Text(
-          category.title,
-          style: Theme.of(context).textTheme.headline6,
-        ),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15.0),
-          gradient: LinearGradient(
-            colors: [
-              category.color.withOpacity(0.5),
-              category.color,
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
+        child: Container(
+          padding: const EdgeInsets.all(15.0),
+          child: Text(
+            category.title,
+            style: Theme.of(context).textTheme.headline6,
+          ),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15.0),
+            gradient: LinearGradient(
+              colors: [
+                category.color.withOpacity(0.5),
+                category.color,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
           ),
         ),
       ),
