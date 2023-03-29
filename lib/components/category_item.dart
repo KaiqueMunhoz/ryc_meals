6c3412e2fedfab205aa8abbdbd898dfc5c3e0a3b
Kaique Munhoz <munhoz13km@gmail.com>
2023-03-28 22:50:54 -0300

    feat: Remove category parameter, for a while

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -14,7 +14,7 @@ class CategoryItem extends StatelessWidget {
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (_) {
-          return CategoriesMealsScreen(category);
+          return CategoriesMealsScreen();
         },
       ),
     );
--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:ryc_meals/models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
-  final Category category;
-
-  const CategoriesMealsScreen(
-    this.category, {
+  const CategoriesMealsScreen({
     Key key,
   }) : super(key: key);
 
@@ -13,10 +9,10 @@ class CategoriesMealsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(category.title),
+        title: Text(''),
       ),
       body: Center(
-        child: Text('Receitas por Categoria ${category.id}'),
+        child: Text('Receitas por Categoria'),
       ),
     );
   }
