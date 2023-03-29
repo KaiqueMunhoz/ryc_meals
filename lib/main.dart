909f7e0cc9746a51af5797caa14401c6832e036f
Kaique Munhoz <munhoz13km@gmail.com>
2023-03-28 21:50:18 -0300

    fix: Change title of application

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DeliMeals',
+      title: 'Vamos Cozinhar?',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
