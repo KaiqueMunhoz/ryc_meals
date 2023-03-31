2b21a0b10923e3dd402085def9c392a2e3bb61ca
Kaique Munhoz <munhoz13km@gmail.com>
2023-03-31 06:47:34 -0300

    feat: Create method _createSwitch

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -4,6 +4,20 @@ import 'package:ryc_meals/components/main_drawer.dart';
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({Key key}) : super(key: key);
 
+  Widget _createSwitch(
+    String title,
+    String subtitle,
+    bool value,
+    Function onChanged,
+  ) {
+    return SwitchListTile(
+      title: Text(title),
+      subtitle: Text(subtitle),
+      value: value,
+      onChanged: onChanged,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
