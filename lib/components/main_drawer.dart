ff0d2aacfba81bc9624ffb8e064d867c9874da80
Kaique Munhoz <munhoz13km@gmail.com>
2023-03-30 22:33:51 -0300

    feat: Create method _createItem

--- lib/components/main_drawer.dart
+++ lib/components/main_drawer.dart
@@ -3,6 +3,23 @@ import 'package:flutter/material.dart';
 class MainDrawer extends StatelessWidget {
   const MainDrawer({Key key}) : super(key: key);
 
+  Widget _createItem(IconData icon, String label) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        size: 26,
+      ),
+      title: Text(
+        label,
+        style: TextStyle(
+            fontFamily: 'RobotoCondensed',
+            fontSize: 24,
+            fontWeight: FontWeight.bold),
+      ),
+      onTap: () {},
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
