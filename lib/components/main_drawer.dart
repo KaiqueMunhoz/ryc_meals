a4f1954738650f436e1d831d8b8389c2136b314c
Kaique Munhoz <munhoz13km@gmail.com>
2023-03-30 22:30:35 -0300

    feat: Add style to Container and Text

--- lib/components/main_drawer.dart
+++ lib/components/main_drawer.dart
@@ -9,7 +9,19 @@ class MainDrawer extends StatelessWidget {
       child: Column(
         children: [
           Container(
-            child: Text('Vamos Cozinhar'),
+            height: 120,
+            width: double.infinity,
+            padding: EdgeInsets.all(20),
+            color: Theme.of(context).accentColor,
+            alignment: Alignment.bottomRight,
+            child: Text(
+              'Vamos Cozinhar?',
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 30,
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
           )
         ],
       ),
