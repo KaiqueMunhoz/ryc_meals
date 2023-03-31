c95bc80c5663080d1ca712bc30b84b9dd2f1f13e
Kaique Munhoz <munhoz13km@gmail.com>
2023-03-30 21:59:13 -0300

    feat: Remove DefaultTabController

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -7,31 +7,11 @@ class TabsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Vamos Cozinhar?'),
-          bottom: TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categorias',
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: 'Favoritos',
-              ),
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: [
-            const CategoriesScreen(),
-            const FavoriteScreen(),
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Vamos Cozinhar?'),
       ),
+      body: Center(),
     );
   }
 }
