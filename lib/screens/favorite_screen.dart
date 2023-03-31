d4b4c2b5ace2b13a2f134d5bf848fa7c15a90901
Kaique Munhoz <munhoz13km@gmail.com>
2023-03-30 21:29:01 -0300

    feat: Create a draft of FavoriteScreen

---
+++ lib/screens/favorite_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class FavoriteScreen extends StatelessWidget {
+  const FavoriteScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Vamos Cozinhar?'),
+      ),
+      body: Center(
+        child: Text(
+          'Minhas Refeições Favoritas',
+        ),
+      ),
+    );
+  }
+}
