796f0e7c993a33097c352b0b543ed3c3f89e1b26
Kaique Munhoz <munhoz13km@gmail.com>
2023-03-30 21:14:23 -0300

    feat: Add second title

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -68,6 +68,7 @@ class MealDetailScreen extends StatelessWidget {
                 },
               ),
             ),
+            _createSectionTitle(context, 'Passos'),
           ],
         ),
       ),
